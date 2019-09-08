83cd15beb60ca1e02d0a1ad3de5098f59e4b837b
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 16:50:19 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def display_board(board)	
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "	
+  puts "-----------"	
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "	
+  puts "-----------"	
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "	
+end
+
+def input_to_index(input)
+  converted_input = (input).to_i 
+  converted_input -= 1
+end 
+
+def valid_move?(board, index)
+  if index < 0 || index > 8
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return false
+  else
+    true
+  end 
+end 
+
+def move(board, index, char)
+  board[index] = char
+end 
+
+def turn(board) 
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, char)
+  else
+    puts "Please enter another number"
+  end
+end 
