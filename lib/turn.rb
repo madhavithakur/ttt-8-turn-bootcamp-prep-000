8ecfd82e03b3368a3a85153ce0938a22f62bb1e9
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 16:59:26 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,14 +36,14 @@ def turn(board)
   end
 end 
 
-def turn (board)
-  puts "Please enter 1-9:"
-  num = gets.chomp
-  index = input_to_index(num)
-  if valid_move?(board, index) == true
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
+# def turn (board)
+#   puts "Please enter 1-9:"
+#   num = gets.chomp
+#   index = input_to_index(num)
+#   if valid_move?(board, index) == true
+#     move(board, index)
+#     display_board(board)
+#   else
+#     turn(board)
+#   end
+# end
